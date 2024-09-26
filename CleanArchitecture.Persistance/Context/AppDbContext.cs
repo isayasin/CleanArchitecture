b5d6f3bb2ec1c74d196f1b818b38df5cd1bf414f
isayasin <isayasinkuru@gmail.com>
2024-09-26 14:42:07 +0300

    User tablosu için identity kütüphanesini ekledik.

--- CleanArchitecture.Persistance/Context/AppDbContext.cs
+++ CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -1,15 +1,24 @@
 ﻿using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Entities;
 using GenericRepository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Persistance.Context;
-public sealed class AppDbContext : DbContext, IUnitOfWork
+public sealed class AppDbContext : IdentityDbContext<User, IdentityRole, string>, IUnitOfWork
 {
     public AppDbContext(DbContextOptions options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+        modelBuilder.Ignore<IdentityRole<string>>();
+        modelBuilder.Ignore<IdentityRoleClaim<string>>();
+        modelBuilder.Ignore<IdentityUserLogin<string>>();
+        modelBuilder.Ignore<IdentityUserToken<string>>();
+        modelBuilder.Ignore<IdentityUserRole<string>>();
+        modelBuilder.Ignore<IdentityUserClaim<string>>();
     }
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
